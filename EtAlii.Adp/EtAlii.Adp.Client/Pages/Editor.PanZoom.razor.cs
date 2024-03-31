3760d0d7c8d0a088e87d4c84d997d629e53fbdff
Peter Vrenken <peter.vrenken@accenture.com>
2024-03-31 11:53:35 +0200

    Added overlay toolpanel

---
+++ EtAlii.Adp/EtAlii.Adp.Client/Pages/Editor.PanZoom.razor.cs
@@ -0,0 +1,230 @@
+﻿using Syncfusion.Blazor.Diagram;
+using Syncfusion.Blazor.Navigations;
+using Orientation = Syncfusion.Blazor.Diagram.Orientation;
+
+namespace EtAlii.Adp.Client.Pages;
+
+public partial class Editor
+{
+    //Defines diagram constraints
+    private DiagramConstraints _constraints;
+
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _zoomInButton = null!;
+    private string _zoomButtonOutItemCssClass = null!;
+
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _zoomOutButton = null!;
+    private string _zoomButtonInItemCssClass = null!;
+
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _panButton = null!;
+    private string _panButtonItemCssClass = null!;
+
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _pointerButton = null!;
+    private string _pointerButtonCssClass = null!;
+    
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _viewButton = null!;
+    private string _viewButtonCssClass = null!;
+    private bool _view;
+
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _centerButton = null!;
+    private string _centerButtonCssClass = null!;
+    private bool _center;
+
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _fitToPageButton = null!;
+    private string _fitButtonCssClass = null!;
+    
+    // ReSharper disable once NotAccessedField.Local
+    private ToolbarItem _resetButton = null!;
+    private string _resetButtonCssClass = null!;
+    private bool _reset;
+    
+    private DiagramInteractions _diagramTool = DiagramInteractions.ZoomPan;
+    
+    private void OnPanClick()
+    {
+        _panButtonItemCssClass = "tb-item-middle tb-item-selected";
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";
+        _pointerButtonCssClass = "tb-item-start";
+        _pointerButtonCssClass = "tb-item-start";
+        _diagramTool = DiagramInteractions.ZoomPan;
+        _view = true;
+        _center = true;
+        _reset = true;
+    }
+
+    private void OnResetClick()
+    {
+        if (_diagramTool == DiagramInteractions.Default)
+        {
+            _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+            _panButtonItemCssClass = "tb-item-start";
+        }
+        else
+        {
+            _pointerButtonCssClass = "tb-item-start";
+            _panButtonItemCssClass = "tb-item-middle tb-item-selected";
+        }
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _resetButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";       
+        _diagram.ResetZoom();
+        _reset = true;
+    }
+
+
+    private void OnFittoPageClick()
+    {
+        if (_diagramTool == DiagramInteractions.Default)
+        {
+            _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+            _panButtonItemCssClass= "tb-item-start";
+        }
+        else
+        {
+            _pointerButtonCssClass = "tb-item-start";
+            _panButtonItemCssClass = "tb-item-middle tb-item-selected";
+        }
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _resetButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";
+        _diagram.FitToPage(new FitOptions(){ Mode = FitMode.Both, Region = DiagramRegion.Content });
+        _reset = false;
+    }
+    private void OnBringintoViewClick()
+    {
+        _panButtonItemCssClass = "tb-item-start";
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";
+        _resetButtonCssClass = "tb-item-start";
+        if (_diagramTool == DiagramInteractions.Default)
+        {
+            _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+        }
+        else
+        {
+            _pointerButtonCssClass= "tb-item-start";
+        }
+        if (_diagram.SelectionSettings.Nodes.Count > 0)
+        {
+            Node node = _diagram.SelectionSettings.Nodes[0];
+            DiagramRect bound = new DiagramRect((node.OffsetX - (node.Width / 2)), node.OffsetY - (node.Height / 2), node.Width, node.Height);
+            _diagram.BringIntoView(bound);
+        }
+        _reset = false;
+    }
+    private void OnBringintoCenterClick()
+    {
+        if (_diagramTool == DiagramInteractions.Default)
+        {
+            _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+        }
+        else
+        {
+            _pointerButtonCssClass = "tb-item-start";
+        }
+        _resetButtonCssClass = "tb-item-start";
+        _panButtonItemCssClass = "tb-item-start";
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";
+        if (_diagram.SelectionSettings.Nodes.Count > 0)
+        {
+            Node node = _diagram.SelectionSettings.Nodes[0];
+            DiagramRect bound = new DiagramRect((node.OffsetX - (node.Width / 2)), node.OffsetY - (node.Height / 2), node.Width, node.Height);
+            _diagram.BringIntoCenter(bound);
+        }
+        _reset = false;
+    }
+    private void OnPointerClick()
+    {
+        _diagramTool = DiagramInteractions.SingleSelect | DiagramInteractions.MultipleSelect;
+        _panButtonItemCssClass = "tb-item-start";
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        if (_diagram.SelectionSettings.Nodes.Count > 0)
+        {
+            _viewButtonCssClass = "tb-item-start";
+            _centerButtonCssClass = "tb-item-start";
+            _fitButtonCssClass = "tb-item-start";
+            _view = false;
+            _center = false;
+        }
+        _resetButtonCssClass = "tb-item-start";
+        _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+        _reset = true;
+    }
+    private TreeInfo GetLayoutInfo(IDiagramObject _, TreeInfo options)
+    {
+        options.EnableSubTree = true;
+        options.Orientation = Orientation.Horizontal;
+        return options;
+    }
+
+    private void OnZoomInItemClick()
+    {
+        if (_diagramTool == DiagramInteractions.Default)
+        {
+            _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+            _panButtonItemCssClass= "tb-item-start";
+        }
+        else
+        {
+            _pointerButtonCssClass = "tb-item-start";
+            _panButtonItemCssClass = "tb-item-middle tb-item-selected";
+        }
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";
+        _resetButtonCssClass = "tb-item-start";
+        _diagram.Zoom(1.2, new DiagramPoint() { X = 100, Y = 100 });
+        _reset = false;
+    }
+    public void ZoomChanged()
+    {
+        _reset = false;
+    }
+    private void OnZoomOutItemClick()
+    {
+        if (_diagramTool == DiagramInteractions.Default)
+        {
+            _pointerButtonCssClass = "tb-item-middle tb-item-selected";
+            _panButtonItemCssClass= "tb-item-start";
+        }
+        else
+        {
+            _pointerButtonCssClass = "tb-item-start";
+            _panButtonItemCssClass = "tb-item-middle tb-item-selected";
+        }
+        _zoomButtonInItemCssClass = "tb-item-start";
+        _zoomButtonOutItemCssClass = "tb-item-start";
+        _viewButtonCssClass = "tb-item-start";
+        _centerButtonCssClass = "tb-item-start";
+        _fitButtonCssClass = "tb-item-start";
+        _resetButtonCssClass = "tb-item-start";
+        _diagram.Zoom(1 / 1.2, new DiagramPoint() { X = 100, Y = 100 });
+        _reset = false;
+    }    
+}
---
+++ EtAlii.Adp/EtAlii.Adp/Data/Migrations/20240330103816_Next Migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EtAlii.Adp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NextMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
