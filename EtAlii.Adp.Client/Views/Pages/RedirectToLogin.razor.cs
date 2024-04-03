7a3abbb451ea25adf7e32c5c4110bd760b0e4461
Peter Vrenken <peter.vrenken@accenture.com>
2024-04-03 23:39:42 +0200

    Improving architectural subdivision.

---
+++ EtAlii.Adp.Client/Views/Pages/RedirectToLogin.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace EtAlii.Adp.Client;
+
+public partial class RedirectToLogin
+{
+    protected override void OnInitialized()
+    {
+        var url = $"Account/Login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}";
+        NavigationManager.NavigateTo(url, forceLoad: true);
+    }
+}
