b61c642c87314a461a37839814f4da1ec81dfe11
Peter Vrenken <peter.vrenken@accenture.com>
2024-04-15 00:14:27 +0200

    Updated GraphQL API.

---
+++ EtAlii.Adp.Client/Editor/_Model/Item.WireExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace EtAlii.Adp.Client;
+
+public static class ItemWireExtensions
+{
+    public static Item ToLocal(this IAddItem_AddItem_Item item)
+    {
+        return new Item
+        {
+            Id = Guid.Parse(item.Id),
+            Name = item.Name,
+            X = (float)item.X,
+            Y = (float)item.Y,
+            W = (float)item.W,
+            H = (float)item.H,
+        };
+    }
+}
---
+++ EtAlii.Adp.Client/Editor/_Model/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace EtAlii.Adp.Client;
+
+public class Item
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; } = string.Empty;
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float W { get; set; }
+    public float H { get; set; }
+}
