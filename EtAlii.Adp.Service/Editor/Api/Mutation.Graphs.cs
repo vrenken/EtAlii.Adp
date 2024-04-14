07f078da89ff8d5e1354aa8c5a2f6d08f3090a79
Peter Vrenken <peter.vrenken@accenture.com>
2024-04-15 00:10:34 +0200

    Moved test data.

--- EtAlii.Adp.Service/Editor/Api/Mutation.Graphs.cs
+++ EtAlii.Adp.Service/Editor/Api/Mutation.Graphs.cs
@@ -14,6 +14,19 @@ public partial class Mutation
         var graph = new Graph { Name = name };
         await dbContext.Graphs.AddAsync(graph);
 
+        var startItem = new Item { Id = Guid.NewGuid(), X = 300, Y = 50, W = 145, H = 60, Name = "Start" };
+        graph.Items.Add(startItem);
+        var initItem = new Item { Id = Guid.NewGuid(), X = 300, Y = 140, W = 145, H = 60, Name = "Init" };
+        graph.Items.Add(initItem);
+        var conditionItem = new Item { Id = Guid.NewGuid(), X = 300, Y = 230, W = 145, H = 60, Name = "Condition" };
+        graph.Items.Add(conditionItem);
+        var printItem = new Item { Id = Guid.NewGuid(), X = 300, Y = 320, W = 145, H = 60, Name = "Print" };
+        graph.Items.Add(printItem);
+        var incrementItem = new Item { Id = Guid.NewGuid(), X = 300, Y = 410, W = 145, H = 60, Name = "Increment" };
+        graph.Items.Add(incrementItem);
+        var endItem = new Item { Id = Guid.NewGuid(), X = 300, Y = 500, W = 145, H = 60, Name = "End" };
+        graph.Items.Add(endItem);
+
         await dbContext.SaveChangesAsync();
 
         await sender.SendAsync(nameof(Subscription.GraphAdded), graph);
