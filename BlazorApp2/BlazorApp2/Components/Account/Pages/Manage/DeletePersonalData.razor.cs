8c48a6245654caa767e32249dd5ff0f7b571dd44
Peter Vrenken <peter.vrenken@accenture.com>
2024-06-02 23:42:43 +0200

    Moved code to code-behind pattern.

---
+++ BlazorApp2/BlazorApp2/Components/Account/Pages/Manage/DeletePersonalData.razor.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BlazorApp2.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Components.Account.Pages.Manage;
+
+public partial class DeletePersonalData
+{
+    private string? message;
+    private ApplicationUser user = default!;
+    private bool requirePassword;
+    [CascadingParameter] private HttpContext HttpContext { get; set; } = default!;
+    [SupplyParameterFromForm] private InputModel Input { get; set; } = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Input ??= new();
+        user = await UserAccessor.GetRequiredUserAsync(HttpContext);
+        requirePassword = await UserManager.HasPasswordAsync(user);
+    }
+
+    private async Task OnValidSubmitAsync()
+    {
+        if (requirePassword && !await UserManager.CheckPasswordAsync(user, Input.Password))
+        {
+            message = "Error: Incorrect password.";
+            return;
+        }
+
+        var result = await UserManager.DeleteAsync(user);
+        if (!result.Succeeded)
+        {
+            throw new InvalidOperationException("Unexpected error occurred deleting user.");
+        }
+
+        await SignInManager.SignOutAsync();
+
+        var userId = await UserManager.GetUserIdAsync(user);
+        Logger.LogInformation("User with ID '{UserId}' deleted themselves", userId);
+
+        RedirectManager.RedirectToCurrentPage();
+    }
+
+    private sealed class InputModel
+    {
+        [DataType(DataType.Password)] public string Password { get; set; } = "";
+    }
+}
---
+++ BlazorApp2/BlazorApp2/Components/Account/Pages/Manage/GenerateRecoveryCodes.razor.cs
@@ -0,0 +1,33 @@
+﻿using BlazorApp2.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Components.Account.Pages.Manage;
+
+public partial class GenerateRecoveryCodes
+{
+    private string? message;
+    private ApplicationUser user = default!;
+    private IEnumerable<string>? recoveryCodes;
+    [CascadingParameter] private HttpContext HttpContext { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        user = await UserAccessor.GetRequiredUserAsync(HttpContext);
+
+        var isTwoFactorEnabled = await UserManager.GetTwoFactorEnabledAsync(user);
+        if (!isTwoFactorEnabled)
+        {
+            throw new InvalidOperationException(
+                "Cannot generate recovery codes for user because they do not have 2FA enabled.");
+        }
+    }
+
+    private async Task OnSubmitAsync()
+    {
+        var userId = await UserManager.GetUserIdAsync(user);
+        recoveryCodes = await UserManager.GenerateNewTwoFactorRecoveryCodesAsync(user, 10);
+        message = "You have generated new recovery codes.";
+
+        Logger.LogInformation("User with ID '{UserId}' has generated new 2FA recovery codes", userId);
+    }
+}
